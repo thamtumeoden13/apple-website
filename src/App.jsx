76164ce757d8b31a6f055fb551784c6bf0ff117d
blackcat13 <detective.blackcat13@gmail.com>
2024-04-16 22:47:12 +0700

    features and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -12,6 +15,9 @@ function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
